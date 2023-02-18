eb139def4fe44afd5869d569d8a351d197b257bc
MaxwellChrist <ribthulu@gmail.com>
2023-02-18 15:15:00 -0500

    added new component for error handling; passed query parameter to edit server to render servers with edit ability; added wildcard route to ensure misc routes are rendered with new error handling component

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 import { ServersService } from '../servers.service';
 
@@ -11,7 +11,7 @@ import { ServersService } from '../servers.service';
 export class ServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
 
-  constructor(private serversService: ServersService, private route: ActivatedRoute) { }
+  constructor(private serversService: ServersService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
     this.server = this.serversService.getServer(parseInt(this.route.snapshot.params.id));
@@ -20,6 +20,11 @@ export class ServerComponent implements OnInit {
         this.server = this.serversService.getServer(parseInt(params.id))
       }
     )
+  }
+
+  onEdit() {
+    this.router.navigate(['edit'], {relativeTo: this.route, queryParamsHandling: 'preserve'})
+
   }
 
 }
